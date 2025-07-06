113393d30137dea2022d38d9134d786ed2770709
Boris <wh1te.l1st.re@gmail.com>
2025-07-06 18:37:06 +0300

    Написана функция генерации графика суммы энергопотребления во времени

--- backend/graphs.py
+++ backend/graphs.py
@@ -1,5 +1,6 @@
 import plotly .graph_objs as go
 import plotly
+import pandas as pd
 
 
 
@@ -37,3 +38,16 @@ def graph_8():
 
 def graph_9():
     pass
+
+
+def plot_total_energy_consumption(df):
+    fig = go.Figure()
+    fig.update_xaxes(range=[df['date'].iloc[-145], df['date'].iloc[-1] + pd.Timedelta(hours=2)])
+    fig.update_yaxes(range=[min(df['Appliances'].tail(145) + df['lights'].tail(145)) - 100,
+                            max(df['Appliances'].tail(145) + df['lights'].tail(145)) + 100])
+    fig.add_trace(go.Scatter(x=df['date'], y=(df['Appliances'] + df['lights']), mode='lines+markers', name=''))
+    fig.update_layout(title='Энергопотребление приборов',
+                      xaxis_title='Дата',
+                      yaxis_title='Энергопотребление',
+                      margin=dict(l=0, r=0, t=30, b=0))
+    fig.update_traces(hoverinfo="all", hovertemplate="Дата: %{x}<br>Потребление: %{y}")
