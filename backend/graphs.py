94faccfc08d4c31b8ea1df3338767a6461c39ec9
Boris <wh1te.l1st.re@gmail.com>
2025-07-06 21:45:05 +0300

    Написана функция генерации графика почасового энергопотребления

--- backend/graphs.py
+++ backend/graphs.py
@@ -54,7 +54,8 @@ def plot_total_energy_consumption(df):
                       xaxis_title='Дата',
                       yaxis_title='Энергопотребление',
                       margin=dict(l=0, r=0, t=30, b=0))
-    fig.update_traces(hoverinfo="all", hovertemplate="Дата: %{x}<br>Потребление: %{y}")
+    fig.update_traces(hoverinfo="all", hovertemplate="Дата: %{x}<br>"
+                                                     "Потребление: %{y}")
 
 
 def plot_appliances_and_lights_energy_consumption(df):
@@ -85,4 +86,77 @@ def plot_appliances_and_lights_energy_consumption(df):
                       legend_orientation="h",
                       legend=dict(x=0.5, xanchor='center'),
                       margin=dict(l=0, r=0, t=105, b=0))
-    fig.update_traces(hoverinfo="all", hovertemplate="Дата: %{x}<br>Потребление: %{y}")
+    fig.update_traces(hoverinfo="all", hovertemplate="Дата: %{x}<br>"
+                                                     "Потребление: %{y}")
+
+
+def plot_hourly_energy_consumption(df):
+    df['total_energy'] = df['Appliances'] + df['lights']
+
+    hourly_sum = df.resample('h', on='date')['total_energy'].sum().reset_index()
+    hourly_appliances = df.resample('h', on='date')['Appliances'].sum().reset_index()
+    hourly_lights = df.resample('h', on='date')['lights'].sum().reset_index()
+
+    fig = make_subplots(rows=2, cols=2,
+                        specs=[[{"rowspan": 2}, {}], [None, {}]],
+                        subplot_titles=("Общее энергопотребление", "Энергопотребление бытовых приборов",
+                                        "Энергопотребление света"))
+
+    fig.update_xaxes(title='Дата',
+                     range=[hourly_sum['date'].iloc[-75],
+                            hourly_sum['date'].iloc[-1] + pd.Timedelta(hours=4)],
+                     row=1,
+                     col=1)
+    fig.update_xaxes(title='Дата',
+                     range=[hourly_appliances['date'].iloc[-50],
+                            hourly_appliances['date'].iloc[-1] + pd.Timedelta(hours=4)],
+                     row=1,
+                     col=2)
+    fig.update_xaxes(title='Дата',
+                     range=[hourly_lights['date'].iloc[-50],
+                            hourly_lights['date'].iloc[-1] + pd.Timedelta(hours=4)],
+                     row=2,
+                     col=2)
+    fig.update_yaxes(title='Энергопотребление',
+                     range=[min(hourly_sum['total_energy'].tail(75)) - 300,
+                            max(hourly_sum['total_energy'].tail(75)) + 300],
+                     zeroline=True,
+                     zerolinewidth=2,
+                     zerolinecolor='#902537',
+                     row=1,
+                     col=1)
+    fig.update_yaxes(title='Энергопотребление',
+                     range=[min(hourly_appliances['Appliances'].tail(50)) - 300,
+                            max(hourly_appliances['Appliances'].tail(50)) + 300],
+                     zeroline=True,
+                     zerolinewidth=2,
+                     zerolinecolor='#902537',
+                     row=1,
+                     col=2)
+    fig.update_yaxes(title='Энергопотребление',
+                     range=[min(hourly_lights['lights'].tail(50)) - 30,
+                            max(hourly_lights['lights'].tail(50)) + 30],
+                     zeroline=True,
+                     zerolinewidth=2,
+                     zerolinecolor='#902537',
+                     row=2,
+                     col=2)
+    fig.add_trace(go.Scatter(x=hourly_sum['date'],
+                             y=hourly_sum['total_energy'],
+                             mode='lines+markers', name=''), 1, 1)
+    fig.add_trace(go.Scatter(x=hourly_appliances['date'],
+                             y=hourly_appliances['Appliances'],
+                             mode='lines+markers', name=''), 1, 2)
+    fig.add_trace(go.Scatter(x=hourly_lights['date'],
+                             y=hourly_lights['lights'],
+                             mode='lines+markers', name=''), 2, 2)
+    fig.update_layout(title=dict(text='Почасовое энергопотребление приборов',
+                                 x=0.5,
+                                 xanchor='center',
+                                 yanchor='top',
+                                 font=dict(size=20)),
+                      legend_orientation="h",
+                      legend=dict(x=0.5, xanchor='center'),
+                      margin=dict(l=0, r=0, t=105, b=0))
+    fig.update_traces(hoverinfo="all", hovertemplate="Дата: %{x}<br>"
+                                                     "Потребление: %{y}")
