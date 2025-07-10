21cbcc0f026025db4e979ae8e8436303fbf2d86a
Ivan <ivandenisovich2005@gmail.com>
2025-07-10 21:48:45 +0300

    Добавлен метод очистки папки для временного хранения загруженных датасетов после завершения программы

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+import shutil
+import os
+
 from backend import app
 
 
 
 def main():
     app.run()
+    clear_folder('backend/datasets')
+
+
+def clear_folder(folder_path):
+    shutil.rmtree(folder_path)
+    os.makedirs(folder_path)
 
 
 if __name__ == '__main__':
